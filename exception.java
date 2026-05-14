699228de5aa21e2ccf06ebd299ef1167ad658b09
MUHAMMADZARYABKHAN-21 <muhammadzaryabkhan21@gmail.com>
2026-05-14 22:38:08 +0500

    Initial commit

---
+++ exception.java
@@ -0,0 +1,20 @@
+public class exception{
+
+
+
+    public static void main(string{},args){
+        try{
+        int x =10;
+        int y=0;
+        int z=x/y;
+        }
+        catch{ArithmeticException e}{
+            System.out.print{"can not divide by zero"};
+
+
+        }
+
+        
+        
+    }
+}
---
+++ main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+   
+
+
+
+
+public class main{
+    public static void main(String[] args){
+
+
+int y;
+System.out.println("enter value for() x");
+Scanner sc=new Scanner(System.in);
+y=sc.nextInt();
+System.out.println("the value for x is "+y);
+double [] x=new double[12];
+
+for(int i=0;i<x.length;i++){
+    System.out.println("enter the value for "+(i+1));
+    x[i]=sc.nextDouble();
+
+    }
+
+
+
+for (int i=0;i<x.length;i++){
+    System.out.println("the value for x is  "+i+"is"+x[i]);
+
+}
+    }
+}
